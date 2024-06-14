919f08ab36a6bdd5f91d18f96bd22de464bc7067
CleySanchez <clement.sanchez.31@gmail.com>
2024-06-14 14:49:17 +0200

    Authentication & Authorization

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const express = require('express');
+
 const mongoose = require('mongoose');
 const Models = require('./models.js');
 
@@ -6,10 +8,15 @@ const Users = Models.User;
 
 mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
 
-const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
  
+const cors = require('cors');
+////app.use(cors());//*
+
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+
 
 const app = express();
 const port = 8080; // Use a single port for the server
@@ -24,6 +31,10 @@ app.use(morgan('common'));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+
+/* rest of code goes here*/
+
 // Authentication setup
 let auth = require('./auth')(app);
 
@@ -36,6 +47,19 @@ require('./passport');
 
 
 // Routes
+
+// Endpoint to get all genres
+app.get('/genre', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  await Movies.distinct('genre.name')
+    .then((genre) => {
+      res.status(200).json(genre);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
+
 
 // Get all movies with JWT authentication
 app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
@@ -76,6 +100,7 @@ app.post('/users', async (req, res) => {
 
 
 
+
 // GET route for the home page
 app.get('/', (req, res) => {
   res.send('Welcome to My Movie App!!!');
@@ -83,36 +108,41 @@ app.get('/', (req, res) => {
 
 // Get a single movie by title with JWT authentication
 app.get('/movies/:title', passport.authenticate('jwt', { session: false }), async (req, res) => {
-  try {
-    const movie = await Movies.findOne({ Title: req.params.title });
-    if (movie) {
-      res.json(movie);
-    } else {
-      res.status(404).send('Movie not found');
-    }
-  } catch (error) {
-    res.status(500).json({ error: 'An error occurred while fetching the movie' });
-  }
+  await Movies.findOne({ title: req.params.title })
+    .then((movie) => {
+      if (movie) {
+        res.json(movie);
+      } else {
+        res.status(404).send('Movie not found');
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
+
 // Get a genre by name with JWT authentication
-app.get('/genres/:name', passport.authenticate('jwt', { session: false }), async (req, res) => {
-  try {
-    const movies = await Movies.find({ 'Genre.Name': req.params.name });
-    if (movies.length > 0) {
-      res.json(movies);
-    } else {
-      res.status(404).send('Genre not found');
-    }
-  } catch (error) {
-    res.status(500).json({ error: 'An error occurred while fetching the genre' });
-  }
+app.get('/genre/:name', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  await Movies.find({ 'genre.name': req.params.name })
+    .then((movies) => {
+      if (movies.length > 0) {
+        res.json(movies);
+      } else {
+        res.status(404).send('Genre not found');
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
 // Get a director by name with JWT authentication
-app.get('/directors/:name', passport.authenticate('jwt', { session: false }), async (req, res) => {
+app.get('/director/:name', passport.authenticate('jwt', { session: false }), async (req, res) => {
   try {
-    const movies = await Movies.find({ 'Director.Name': req.params.name });
+    const movies = await Movies.find({ 'director.name': req.params.name });
     if (movies.length > 0) {
       res.json(movies);
     } else {
@@ -124,13 +154,13 @@ app.get('/directors/:name', passport.authenticate('jwt', { session: false }), as
 });
 
 // Update user info with JWT authentication
-app.put('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
+app.put('/users/:username', passport.authenticate('jwt', { session: false }), async (req, res) => {
   // CONDITION TO CHECK ADDED HERE
-  if(req.user.Username !== req.params.Username){
+  if(req.user.Username !== req.params.username){
       return res.status(400).send('Permission denied');
   }
   // CONDITION ENDS
-  await Users.findOneAndUpdate({ Username: req.params.Username }, {
+  await Users.findOneAndUpdate({ username: req.params.username }, {
       $set:
       {
           Username: req.body.Username,
