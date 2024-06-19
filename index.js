fe25a298c268ac66dd5ca516562e28e66dfc54ae
CleySanchez <clement.sanchez.31@gmail.com>
2024-06-19 19:18:59 +0200

    Security of data,validation and ethic7

--- index.js
+++ index.js
@@ -17,7 +17,18 @@ const Users = Models.User;
 
 // MongoDB connection
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+ 
+//const connectionUri = process.env.CONNECTION_URI;//
 
+/* mongoose.connect(connectionUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  }); 
+ */
+ 
 
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
